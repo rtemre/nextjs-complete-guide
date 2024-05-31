96486f3b4bd813b813ca325b9ee4963d09fbb8e2
Raman Temre <ramantemre2110@gmail.com>
2024-05-31 19:12:51 +0530

    feat: Dynamic metadata

--- 07-optimizations/app/feed/page.js
+++ 07-optimizations/app/feed/page.js
@@ -1,5 +1,16 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// For Dynamic metadata Nextjs looks for generateMetadata() function.
+// This generateMetadata(config) has parameters such as ({ params, searchParams })
+export async function generateMetadata(config) {
+  const post = await getPosts();
+  const numberOfPosts = post.length;
+  return {
+    title: `Browse our ${numberOfPosts} posts`,
+    description: "Browse our all posts.",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
