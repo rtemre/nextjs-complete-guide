1fa1fc01330c195da745900876d6864493cafa6f
Raman Temre <ramantemre2110@gmail.com>
2024-05-31 18:46:34 +0530

    feat: Image Optimization

--- 07-optimizations/components/header.js
+++ 07-optimizations/components/header.js
@@ -1,11 +1,16 @@
-import logo from '@/assets/logo.png';
-import Link from 'next/link';
+import logo from "@/assets/logo.png";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          alt="Mobile phone with posts feed on it"
+          sizes="10vw"
+        />
       </Link>
       <nav>
         <ul>
@@ -13,7 +18,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
