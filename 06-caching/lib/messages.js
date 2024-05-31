3d53fb9e8ba6d8cf109408199c9c6043e0d8088e
Raman Temre <ramantemre2110@gmail.com>
2024-05-31 14:45:34 +0530

    feat: Full route cache, revalidatePath & revalidateTag

--- 06-caching/lib/messages.js
+++ 06-caching/lib/messages.js
@@ -1,6 +1,6 @@
-import sql from 'better-sqlite3';
-
-const db = new sql('messages.db');
+import sql from "better-sqlite3";
+import { cache } from "react";
+const db = new sql("messages.db");
 
 function initDb() {
   db.exec(`
@@ -13,10 +13,10 @@ function initDb() {
 initDb();
 
 export function addMessage(message) {
-  db.prepare('INSERT INTO messages (text) VALUES (?)').run(message);
+  db.prepare("INSERT INTO messages (text) VALUES (?)").run(message);
 }
 
-export function getMessages() {
-  console.log('Fetching messages from db');
-  return db.prepare('SELECT * FROM messages').all();
-}
+export const getMessages = cache(function getMessages() {
+  console.log("Fetching messages from db");
+  return db.prepare("SELECT * FROM messages").all();
+});
