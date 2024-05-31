e504d80c40542e6928e385785d32a8f8d41c7eda
Raman Temre <ramantemre2110@gmail.com>
2024-05-31 11:37:24 +0530

    feat: Request Memoization

--- 06-caching/backend/app.js
+++ 06-caching/backend/app.js
@@ -1,13 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/messages', (req, res) => {
-  const requestSource = req.headers['x-id'];
-  console.log(`${new Date().toISOString()}: EXECUTING /messages on backend from ${requestSource}`);
+app.get("/messages", (req, res) => {
+  const requestSource = req.headers["x-id"];
+  // To Achive Request Memoization - make the same request from frontend by removing header config which make differences
+  // Then after the above change "resquestSource" will undefined
+  console.log(
+    `${new Date().toISOString()}: EXECUTING /messages on backend from ${requestSource}`
+  );
   res.json([
-    { id: 1, text: 'Hello World' },
-    { id: 2, text: 'Another message from the separate backend' },
+    { id: 1, text: "Hello World" },
+    { id: 2, text: "Another message from the separate backend" },
   ]);
 });
 
