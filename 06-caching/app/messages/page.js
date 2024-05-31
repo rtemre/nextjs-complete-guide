ebbe4ba203b22c19d01ead104799e3fdda8b6dd6
Raman Temre <ramantemre2110@gmail.com>
2024-05-31 11:58:38 +0530

    feat: Data Caching & controlling

--- 06-caching/app/messages/page.js
+++ 06-caching/app/messages/page.js
@@ -1,6 +1,11 @@
 import Messages from "@/components/messages";
+// import { unstable_noStore } from "next/cache";
+
+// export const dynamic = "force-dynamic";
+// export const revalidate = 5;
 
 export default async function MessagesPage() {
+  //   unstable_noStore();
   const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
